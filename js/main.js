7b23a31d10111f0e75ec300c7da0a45de56f3f49
hillaryjm <hillarymacfarlane@Hillarys-MacBook-Pro.local>
2016-03-21 14:15:44 -0700

    major tweaks
    centre the timeline, added query

---
+++ js/main.js
@@ -0,0 +1,59 @@
+$('.1874-btn').click(
+  function(){
+    $('#left-1874').toggleClass('hidden')
+  }
+)
+
+$('.1902-btn').click(
+  function(){
+    $('#left-1902').toggleClass('hidden')
+  }
+)
+
+$('.1911-btn').click(
+  function(){
+    $('#left-1911').toggleClass('hidden')
+  }
+)
+
+$('.1921-btn').click(
+  function(){
+    $('#left-1921').toggleClass('hidden')
+  }
+)
+
+$('.1940-btn').click(
+  function(){
+    $('#left-1940').toggleClass('hidden')
+  }
+)
+
+$('.1853-btn').click(
+  function(){
+    $('#right-1853').toggleClass('hidden')
+  }
+)
+
+$('.1877-btn').click(
+  function(){
+    $('#right-1877').toggleClass('hidden')
+  }
+)
+
+$('.1905-btn').click(
+  function(){
+    $('#right-1905').toggleClass('hidden')
+  }
+)
+
+$('.1918-btn').click(
+  function(){
+    $('#right-1918').toggleClass('hidden')
+  }
+)
+
+$('.1926-btn').click(
+  function(){
+    $('#right-1926').toggleClass('hidden')
+  }
+)
